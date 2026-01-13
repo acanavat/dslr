8e329d0b50724ca1976a756e9923fbb603ec8eb8
isibio <ivanSibio@protonmail.com>
2026-01-13 18:51:02 +0100

    updated histogram.py, creating graph

--- srcs/histogram.py
+++ srcs/histogram.py
@@ -1,7 +1,44 @@
+import csv
+import array
+import argparse
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pand
+
+
 #
 # main
 def main():
-	print("Hello world")
+	# Parsing en utilisant 'argparse'
+	parser = argparse.ArgumentParser()
+	parser.add_argument("datasetPath", type=str)
+	args = parser.parse_args()
+
+	# Extraction du dataset
+
+	dataField	= pand.read_csv(args.datasetPath);
+
+	#
+	# Print
+	houses = getHouseMap(dataField);
+	for house in houses:
+		print ("__ {}".format(house))
+		test = [row["Divination"] for row in houses[house] if pand.notnull(row["Divination"])]
+		plt.hist(tuple(test))
+	plt.show()
+
+def getHouseMap(dataField : pand.core.frame.DataFrame) -> {}:
+	houseMap = {};
+	for index, row in dataField.iterrows():
+		if (row is None):
+			continue ;
+
+		key = row['Hogwarts House']
+		if key not in houseMap:
+			houseMap[key] = [];
+		houseMap[key].append(row);
+	return (houseMap);
+
 
 #
 # Main guard
